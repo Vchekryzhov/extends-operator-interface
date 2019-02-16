6233a1f3302e80000ffae53eda75bad74576579d
C.Viktor <vchekryzhov@ya.ru>
2019-02-16 22:18:09 +0300

    update

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,17 +2,22 @@ require "redis"
 
 class DataController < ApplicationController
 
-  def set
-    redis = Redis.new
-    redis.set(id, data)
-    render json: {}, status: 200
-  end
-
   def get
     redis = Redis.new
-    result = redis.get(id)
-    data = JSON.parse(result.gsub("=>",":")).map{|key, value|[key, value]} if result
+    data = []
+    ids.each do |id_data|
+          result = redis.get(id_data)
+          data.push JSON.parse(result.gsub("=>",":")).map{|key, value|[key, value]} if result
+    end
     render json: {data: data}, status:200
+  end
+
+  def set_db
+    redis = Redis.new
+    data_names.each do |data_name|
+        redis.set("#{data_name}_#{id}", data_by_name(data_name))
+    end
+    render json: {}, status: 200
   end
 
   private
@@ -21,10 +26,19 @@ class DataController < ApplicationController
     params[:id]
   end
 
-  def data
+  def ids
+    params[:ids]
+  end
+
+  def data_names
+    params[:data_name]
+  end
+
+  def data_by_name(name)
     result = params.except(:action, :controller)
     result.delete(:id)
-    result.permit!
+    result.delete(:data_name)
+    result.permit!.require(name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'infos/:id', to: 'info#show'
   get '/set', to: 'data#set'
-  get '/get/:id', to: 'data#get'
+  get '/get/', to: 'data#get'
+  get '/set_db', to: 'data#set_db'
 end
