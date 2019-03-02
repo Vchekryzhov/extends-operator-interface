8b1d48afb70babc22d6b6782ef7a54693855d139
C.Viktor <vchekryzhov@ya.ru>
2019-03-02 20:15:18 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'info#index'
   get 'infos/:id', to: 'info#show', as: 'info'
   get 'infos', to: 'info#index'
   get '/set', to: 'data#set'
   get '/get/', to: 'data#get'
   get '/set_db', to: 'data#set_db'
-  post 'selected_locale', to:'application#selected_locale'
+  post 'selected-locale', to:'application#selected_locale'
 end
