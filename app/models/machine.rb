c759c3a790371ee541e4436b614d9a6b9bafb5fd
Sodi1 <mydeardrummer@gmail.com>
2019-03-23 16:58:31 +0200

    add stand enum

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,5 +2,5 @@ class Machine < ApplicationRecord
   mount_uploader :image, MachinePhotoUploader
   extend Enumerize
 
-  enumerize :machine_type, in: [:milling, :turn, :other]
+  enumerize :machine_type, in: [:milling, :turn, :other, :stand]
 end
