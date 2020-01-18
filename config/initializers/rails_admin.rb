09e2f16704e5f20898260cb0f79af43b513c24e9
C.Viktor <vchekryzhov@ya.ru>
2020-01-18 20:39:39 +0300

    asdasd

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,5 @@
 RailsAdmin.config do |config|
-  config.parent_controller = '::RailsAdminCustomController'
+  # config.parent_controller = '::RailsAdminCustomController'
 
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
