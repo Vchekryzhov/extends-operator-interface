7c2da26d745532ace962692bfd5ce883dd223a6d
C.Viktor <vchekryzhov@ya.ru>
2020-03-20 01:12:58 +0300

    inputs

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,16 @@
 class Request < ApplicationRecord
   searchkick
+  after_create :create_inputs
+  def create_inputs
+    request["inputs"].map do |i|
+        Input.create(
+          device_id: i['device_id'],
+          group_input_id: i['group_id'],
+          values: i['value'],
+          client_time: i['timestamp'],
+          name: i['name'],
+          request_id: r.id
+        )
+    end if r.request["inputs"].present?
+  end
 end
