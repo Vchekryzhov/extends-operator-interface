89758596ea475708961d0049c847655f426b780b
Виктор <vchekryzhov@ya.ru>
2020-09-12 18:11:57 +0300

    monkey

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,13 +1,13 @@
 RailsAdmin.config do |config|
 
+RailsAdmin::ApplicationController.class_eval do
+  skip_before_action :verify_authenticity_token
+end
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
       username == 'ksu' && password == 'ncsystems'
     end
   end
-
-  config.parent_controller = '::ApplicationController'
-
 
   config.model 'Machine' do
     object_label_method do
