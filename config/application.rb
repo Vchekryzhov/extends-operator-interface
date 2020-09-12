290129bcc7ee6466d5f0117f2ffaf03debf7f27f
Виктор <vchekryzhov@ya.ru>
2020-09-12 18:00:42 +0300

    secure

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Untitled
     config.load_defaults 5.2
     config.time_zone = 'Moscow'
     config.active_record.default_timezone = :local # Or :utc
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use Rack::MethodOverride
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
