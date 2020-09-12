f66a58dae8da034caa4fc91fa9cf8f7dcc915016
Виктор <vchekryzhov@ya.ru>
2020-09-12 17:56:36 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://dimapriv.ru']
+    origins ['http://dimapriv.ru', 'http://localhost:3000']
     resource '*', credentials: true, headers: :any, methods: %i(get post put patch delete options head)
   end
 end
